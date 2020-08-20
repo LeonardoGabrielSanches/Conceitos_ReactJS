169b6213b9670b7b8ce32ea8e74f8dff97d5b3a6
Leonardo Sanches <sleonardogabriel@gmail.com>
2020-08-20 20:56:12 -0300

    Add api connection

---
+++ README.md
@@ -0,0 +1,5 @@
+# Go Stack 13
+
+## Conceitos ReactJS
+
+Nível 1 - Front-end com ReactJS
--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpeg';
 
 import Header from './components/Header';
 
 function App() {
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('/projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
 
     function handleAddProject() {
         setProjects([...projects, `${Date.now()}`]);
@@ -15,11 +21,8 @@ function App() {
     return (
         <>
             <Header title="Projects" />
-
-            <img width={300} src={backgroundImage} />
-
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
